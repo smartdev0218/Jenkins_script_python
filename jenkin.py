4a65e095a3e7dbdc53711b170d72c55789f404ed
antonio krijan <antonio.krijan0218@gmail.com>
2023-05-17 10:38:44 +0100

    change

--- jenkin.py
+++ jenkin.py
@@ -1,17 +1,11 @@
 import os
 
-# product_name = os.environ['ProductName']
-# application_name = os.environ['ApplicationName']
-# jira_db = os.environ['JiraDb']
-# target_url = os.environ['TargetURL']
-product_name = "New_Jenkins_Python"
-application_name = "ApplicationName"
-jira_db = "JiraDb"
-target_url = "TargetURL"
+product_name = os.environ['ProductName']
+application_name = os.environ['ApplicationName']
+jira_db = os.environ['JiraDb']
+target_url = os.environ['TargetURL']
 
-# repo_name = product_name + ".git"
 repo_name = product_name
-# os.system("git clone --bare https://github.com/smartdev0218/Jenkins_Python.git " + repo_name)
 
 os.system("git clone https://github.com/smartdev0218/Jenkins_Python.git " + repo_name)
 os.chdir(repo_name)
@@ -19,8 +13,6 @@ os.system("git push --mirror https://github.com/smartdev0218/" + repo_name)
 os.chdir("..")
 
 config_file_path = os.path.join(repo_name, "API", "Conig.yaml")
-# with open(config_file_path, "r") as config_file:
-#   config_data = config_file.read()
 
 file_path = open(config_file_path, "rt")
 
@@ -29,9 +21,6 @@ config_file = file_path.read()
 config_file = config_file.replace(product_name, "ProductNamePlaceholder")
 config_file = config_file.replace(application_name, "ApplicationNamePlaceholder")
 config_file = config_file.replace(target_url, "TargetUrlPlaceholder")
-
-# with open(config_file_path, "w") as config_file:
-#   config_file.write(config_data)
 
 file_path = open(config_file_path, "wt")
 file_path.write(config_file)
@@ -44,9 +33,6 @@ key_folder_path = os.path.join(repo_name, "key")
 os.makedirs(key_folder_path)
 
 key_file_path = os.path.join(key_folder_path, "encrypted_key")
-
-# with open(key_file_path, "w") as key_file:
-# key_file.write(encrypted_key)
 
 key_file = open(key_file_path, "wt")
 key_file.write(encrypted_key)
