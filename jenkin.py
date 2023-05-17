bdf02380e6029cc5f62e168f6be9f91c0bb3d5f1
antonio krijan <antonio.krijan0218@gmail.com>
2023-05-17 10:03:53 +0100

    change

--- jenkin.py
+++ jenkin.py
@@ -5,24 +5,33 @@ application_name = os.environ['ApplicationName']
 jira_db = os.environ['JiraDb']
 target_url = os.environ['TargetURL']
 
-repo_name = product_name + ".git"
-os.system("git clone --bare https://github.com/smartdev0218/Jenkins_Python.git " + repo_name)
+# repo_name = product_name + ".git"
+repo_name = product_name
+# os.system("git clone --bare https://github.com/smartdev0218/Jenkins_Python.git " + repo_name)
 
-os.system("git clone --mirror https://github.com/smartdev0218/Jenkins_Python.git")
-os.chdir("Jenkins_Python.git")
-os.system("git push --mirror ../" + repo_name)
+os.system("git clone https://github.com/smartdev0218/Jenkins_Python.git " + repo_name)
+os.chdir("Jenkins_Python")
+os.system("git push --mirror https://github.com/smartdev0218/" + repo_name)
 os.chdir("..")
 
 config_file_path = os.path.join(repo_name, "API", "Conig.YAML")
-with open(config_file_path, "r") as config_file:
-  config_data = config_file.read()
-  
-config_data = config_data.replace("ProductNamePlaceholder", product_name)
-config_data = config_data.replace("ApplicationNamePlaceholder", application_name)
-config_data = config_data.replace("TargetUrlPlaceholder", target_url)
+# with open(config_file_path, "r") as config_file:
+#   config_data = config_file.read()
 
-with open(config_file_path, "w") as config_file:
-  config_file.write(config_data)
+file = open(config_file_path, "rt")
+
+config_file = file.read()
+
+config_data = config_data.replace(product_name, "ProductNamePlaceholder")
+config_data = config_data.replace(application_name, "ApplicationNamePlaceholder")
+config_data = config_data.replace(target_url, "TargetUrlPlaceholder")
+
+# with open(config_file_path, "w") as config_file:
+#   config_file.write(config_data)
+
+file = open('config.yaml', "wt")
+file.write(config_file)
+file.close()
 
 key = product_name + "_" + application_name
 encrypted_key = "thisisademooauthkey"
@@ -37,4 +46,4 @@ with open(key_file_path, "w") as key_file:
 os.chdir(repo_name)
 os.system("git add .")
 os.system("git commit -m 'Adding updated Config.YAML and encrypted key'")
-os.system("git push")
+os.system("git push --mirror https://github.com/smartdev0218/" + repo_name)
